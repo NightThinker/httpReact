10da21e1e8a8052c19e278301e537e52bb4a0570
jiraphon <jiraphon@sirimedia.com>
2018-05-08 18:00:37 +0700

    transfroming data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,14 +14,21 @@ class Blog extends Component {
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'May'
+                    }
+                });
+                this.setState({posts: updatedPosts});
                 console.log(response.data);
             });
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} key={post.id} />
+            return <Post  key={post.id} title={post.title} author={post.author} />
         });
         return (
             <div>
