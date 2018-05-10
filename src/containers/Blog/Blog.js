0603b79ec1399741888a57cd6f326809d85979cf
jiraphon <jiraphon@sirimedia.com>
2018-05-10 17:14:40 +0700

    /posts/1

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -37,7 +37,7 @@ class Blog extends Component {
                 <Route path="/new-post" exact render={() => <h1>New</h1>} /> */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" exact component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Route path="/posts/:id" exact component={FullPost} />
             </div>
         );
     }
