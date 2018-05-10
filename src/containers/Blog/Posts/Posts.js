577900b7fd7e78f77fca9f5aeaf0bd840349a30a
jiraphon <jiraphon@sirimedia.com>
2018-05-10 17:02:24 +0700

    Passing Route Parameter

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,16 +1,6 @@
-// import FullPost from '../../components/FullPost/FullPost';
-// import NewPost from '../../components/NewPost/NewPost';
-
-
-/* <section>
-<FullPost id={this.state.selectedPostId} />
-</section>
-<section>
-<NewPost />
-</section> */
-
 import React, {Component} from 'react';
 import axios from '../../../axios';
+import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -47,11 +37,14 @@ class Posts extends Component {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong!!!</p>
     if(!this.state.error) {
         posts = this.state.posts.map(post => {
-            return <Post  
-                key={post.id} 
-                title={post.title} 
-                author={post.author} 
-                clicked={() => this.postSelectedHandler(post.id)} />
+            return (
+              <Link to={'/' + post.id} key={post.id}>
+                <Post  
+                  title={post.title} 
+                  author={post.author} 
+                  clicked={() => this.postSelectedHandler(post.id)} />
+              </Link>
+            );
         });
     }
     return (
